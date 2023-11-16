45230590378b2f7c0b2fd45a94d116bd753583fb
Kaan Şengün <427642@ogr.ktu.edu.tr>
2023-11-16 10:32:02 +0300

    Finish

--- classInheritance/classInheritance.h
+++ classInheritance/classInheritance.h
@@ -5,8 +5,99 @@
 //  Created by Kaan Şengün on 15.11.2023.
 //
 
-#ifndef classInheritance_h
-#define classInheritance_h
+#include <iostream>
+using namespace std;
+/*
+ 
+ compile-time:(syntax hatasını kontrol eder) function overloading= fonksiyonların parametrelerinin farklı olması &operator overloading
+ run-time;(çalışırkenki hatalar) function overriding=fonksiyonların çiğnenmesi,miraslanan fonksiyonların aynı olması
 
+ 
+ */
+class baseClass{
+public:
+    int x;
 
-#endif /* classInheritance_h */
+    void setX(int mainX){
+        x=mainX;
+    };
+    int getX()const{
+        return x;
+    }
+    void print(){
+        cout<<"X:"<<x<<endl;
+    }
+    baseClass(){
+        
+    }
+    baseClass(int mainX=0){
+        x=mainX;
+        cout<<"Base class constructor worked."<<endl;
+    }
+    ~baseClass(){
+        cout<<"Base class destructor worked."<<endl;
+    }
+    baseClass(const baseClass &oth){
+        x=oth.x;
+        cout<<"Base class copy constructor worked."<<endl;
+    }
+    
+};
+
+class derivedClass: public baseClass{
+public:
+    int y;
+    void setY(int mainY){
+        y=mainY;
+    }
+    int getY()const {
+        return y;
+    }
+    void print(){
+        baseClass::print();
+        cout<<"Y:"<<y<<endl;
+    }
+    derivedClass(int mainX=0,int mainY=0):baseClass(mainX){
+        //x=mainX;
+        y=mainY;
+        cout<<"Derived class constructor worked."<<endl;
+    }
+    ~derivedClass(){
+        cout<<"Derived class destructor worked."<<endl;
+    }
+    derivedClass(const derivedClass &oth):baseClass(oth){
+        //x=oth.x;
+        y=oth.y;
+        cout<<"Derived class copy constructor worked."<<endl;
+    }
+};
+class AClass{
+public:
+    int a;
+    AClass(){
+        cout<<"AClass constructor worked."<<endl;
+    }
+    ~AClass(){
+        cout<<"AClass destructor worked."<<endl;
+    }
+};
+class BClass{
+public:
+    int b;
+    BClass(){
+        cout<<"BClass constructor worked."<<endl;
+    }
+    ~BClass(){
+        cout<<"BClass destructor worked."<<endl;
+    }
+};
+class CClass:public AClass,public BClass{
+public:
+    CClass()//:AClass()
+    {
+        cout<<"CClass constructor worked."<<endl;
+    }
+    ~CClass(){
+        cout<<"CClass destructor worked."<<endl;
+    }
+};
--- classInheritance/main.cpp
+++ classInheritance/main.cpp
@@ -6,9 +6,33 @@
 //
 
 #include <iostream>
+#include "classInheritance.h"
+using namespace std;
+
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+/*    derivedClass obj1;
+      baseClass obj2;
+      obj2.setX(3);
+      obj1.setX(5);
+      obj1.print();
+      obj1.setY(12);
+      obj1.print2();*/
+    baseClass obj1(5);//constructorlarda yukarıdan aşağıya bir yapım var
+    derivedClass obj2(8,3);//destructorlarda aşağıdan yukarı bir yıkım söz konusu
+    derivedClass obj3(obj2);
+    
+    obj1.print();
+    obj2.print();
+    
+    /*
+    cout<<"For base; X:"<<obj1.getX()<<endl;
+    cout<<"For derived; X:"<<obj2.getX()<<" Y:"<<obj2.getY()<<endl;
+    */
+    /*CClass cObject;
+    cObject.a=5;
+    cObject.b=10;*/
     return 0;
 }
