1872422fefe08d631185a59cc7ff23992eb013b9
Kaan Şengün <427642@ogr.ktu.edu.tr>
2023-11-16 10:31:51 +0300

    Finish

---
+++ classInheritance/classInheritance.h
@@ -0,0 +1,12 @@
+//
+//  classInheritance.h
+//  classInheritance
+//
+//  Created by Kaan Şengün on 15.11.2023.
+//
+
+#ifndef classInheritance_h
+#define classInheritance_h
+
+
+#endif /* classInheritance_h */
